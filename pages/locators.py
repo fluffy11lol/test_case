d304103c7e1c5cb755c45e3bdfb485de04853de2
fluffy11 <d.matsiushenka@gmail.com>
2022-09-01 18:17:15 +0300

    page object ideal

---
+++ pages/locators.py
@@ -0,0 +1,7 @@
+class MainPageLocators:
+	LOGIN_LINK = ("css selector", "#login_link")
+
+
+class LoginPageLocators:
+	LOGIN_FORM_LINK = ("css selector", "#login_form")
+	REGISTER_FORM_LINK = ("css selector", "#register_form")
