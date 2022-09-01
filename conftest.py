5bfe9633eda8b0e76cbbd6711a6c4e0da1fd6e6c
fluffy11 <d.matsiushenka@gmail.com>
2022-09-01 17:24:19 +0300

    assertion added

--- conftest.py
+++ conftest.py
@@ -27,13 +27,13 @@ def browser(request):
 		options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
 		print("\nstart chrome browser for test..")
 		browser = webdriver.Chrome(options=options)
-		browser.implicitly_wait(5)
+		browser.implicitly_wait(3)
 	elif browser_name == "firefox":
 		fp = webdriver.FirefoxProfile()
 		fp.set_preference("intl.accept_languages", user_language)
 		print("\nstart firefox browser for test..")
 		browser = webdriver.Firefox(firefox_profile=fp)
-		browser.implicitly_wait(5)
+		browser.implicitly_wait(3)
 	else:
 		raise pytest.UsageError("--browser_name should be chrome or firefox")
 	yield browser
