d933ef076843f36c8eaaa45eb5b10f2a3f557cc9
fluffy11 <90965954+fluffy11lol@users.noreply.github.com>
2022-09-02 20:26:51 +0300

    Delete old_test directory

--- old_test/pages/locators.py
+++
@@ -1,7 +0,0 @@
-class MainPageLocators:
-	LOGIN_LINK = ("css selector", "#login_link")
-
-
-class LoginPageLocators:
-	LOGIN_FORM_LINK = ("css selector", "#login_form")
-	REGISTER_FORM_LINK = ("css selector", "#register_form")
